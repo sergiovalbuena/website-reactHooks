9163590d253b8f8ace02a26618e9306af793232a
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-26 12:12:21 -0500

    heroSection, styledComponent added

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from "gatsby";
+import styled from 'styled-components';
+
+function HeroSection(){
+    return(
+        <Wrapper>
+            <ContentWrapper>
+                <TextWrapper>
+                <img src="/images/logos/logo.svg" alt="logo" />
+                <Title>Hi, <br/> people</Title>
+                <Description>Welcome to your new Gatsby site.</Description>
+                <Link to="/page-2/">Go to page 2</Link> <br />
+                </TextWrapper>
+            </ContentWrapper>
+        </Wrapper>
+    )
+}
+
+export default HeroSection;
+
+const Wrapper = styled.div`
+    background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`;
+const ContentWrapper = styled.div`
+    max-width: 1234px;
+    margin: 0 auto;
+    padding: 200px 30px;
+`;
+
+const TextWrapper = styled.div`
+    max-width: 360px;
+    display: grid;
+    gap: 30px;
+`;
+
+const Title = styled.h1`
+    font-weight: bold;
+    font-size: 60px;
+    color: white;
+`;
+
+const Description = styled.p `
+    font-size: 17px;
+    line-height: 130%;
+`;
