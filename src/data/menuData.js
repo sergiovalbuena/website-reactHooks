3f9243fd5675eba4e30d4f2858c3650b6bf1e7e2
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-27 21:58:39 -0500

    tooltip added

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -5,3 +5,10 @@ export const menuData = [
     {title: "", icon: "/images/icons/search.svg", link:"/search"},
     {title: "", icon: "/images/icons/account.svg", link:"/account"},
 ];
+
+export const tooltipData = [
+    {title: "Profile", icon: "/images/icons/profile.svg", link:"/profile"},
+    {title: "Settings", icon: "/images/icons/settings.svg", link:"/settings"},
+    {title: "Sign Out", icon: "/images/icons/signout.svg", link:"/logout"},
+
+]
